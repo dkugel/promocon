5bcac9ebc7844c11d1a1337152a1996813909639
Diego <daherrerac@gmail.com>
2018-09-30 22:41:16 -0500

    Cambios
    Actualización  móvil y desktop

---
+++ enviar.php
@@ -0,0 +1,26 @@
+<?php        
+	$name = isset($_POST['name'])? $_POST['name'] : NULL;
+	$email = isset($_POST['email'])? $_POST['email'] : NULL;
+	$phone = isset($_POST['phone'])? $_POST['phone'] : NULL;
+	$message = isset($_POST['message'])? $_POST['message'] : NULL;
+
+    $para = 'dcomercial@promocon.net';
+    $titulo = 'Nuevo contacto Web';
+    $header = 'De: ' . $email;    
+    $cuerpo  = "Nombre: $name\n";
+    $cuerpo .= "E-Mail: $email\n";
+    $cuerpo .= "telefono: $phone\n";      
+    $cuerpo .= "Mensaje: $message\n";
+        
+    if (isset($_POST['submit'])) {
+        if (mail($para, $titulo, $cuerpo, $header)) {
+            echo "<script language='javascript'>
+            alert('Mensaje enviado, muchas gracias.');
+            window.location.href = 'http://www.promocon.net';
+            </script>";
+            }
+        else {
+            echo 'Falló el envio';
+        }
+    }
+?>
