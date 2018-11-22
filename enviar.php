541a914ca618f4dc81be572ec970b4a5834a82c5
Diego Herrera <diego.herrera@velezcaicedo.com>
2018-11-22 15:12:54 -0500

    Cambios
    agrega captcha

--- enviar.php
+++ enviar.php
@@ -1,4 +1,5 @@
-<?php        
+<?php    
+    session_start();     
 	$name = isset($_POST['name'])? $_POST['name'] : NULL;
 	$email = isset($_POST['email'])? $_POST['email'] : NULL;
 	$phone = isset($_POST['phone'])? $_POST['phone'] : NULL;
@@ -11,16 +12,25 @@
     $cuerpo .= "E-Mail: $email\n";
     $cuerpo .= "telefono: $phone\n";      
     $cuerpo .= "Mensaje: $message\n";
-        
-    if (isset($_POST['submit'])) {
-        if (mail($para, $titulo, $cuerpo, $header)) {
-            echo "<script language='javascript'>
-            alert('Mensaje enviado, muchas gracias.');
-            window.location.href = 'http://www.promocon.net';
-            </script>";
+    
+    if($_POST['codigo'] == $_SESSION['captcha']){
+        if (isset($_POST['submit'])) {
+            if (mail($para, $titulo, $cuerpo, $header)) {
+                echo "<script language='javascript'>
+                alert('Mensaje enviado, muchas gracias.');
+                window.location.href = 'http://www.promocon.net';
+                </script>";
+                }
+            else {
+                echo 'Falló el envio';
             }
-        else {
-            echo 'Falló el envio';
         }
+    }else{
+        echo "<script language='javascript'>
+                alert('Código erroneo, intente nuevamente.');
+                window.location.href = 'http://www.promocon.net/contacto.html';
+                </script>";
     }
+        
+       
 ?>
